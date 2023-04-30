8d5efb3698c21fd8462d629bae0731f36cbe4ae1
BinhPhanVan <phanvanbinh192837@gmail.com>
2023-04-30 16:51:59 +0700

    Implement frontend for connect with api resume

--- FrontEnd/src/store/index.js
+++ FrontEnd/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./AuthSlice";
+import resumeReducer from "./ResumeSlice";
 export default configureStore({
   reducer: {
     auth: authReducer,
+    resume : resumeReducer,
   },
 });
 
