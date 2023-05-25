14e9ae2e0f1e3d56ff99667a7a3bb0ed5c9c0d15
prd-binh-phan-van <binh.phan@paradox.ai>
2023-05-25 17:58:26 +0700

    Implement frontend for firebase and chat

--- FrontEnd/src/components/pages/Chat/SendMessage.js
+++ FrontEnd/src/components/pages/Chat/SendMessage.js
@@ -6,8 +6,26 @@ import SendIcon from '@mui/icons-material/Send';
 import LinkIcon from '@mui/icons-material/Link';
 import ImageIcon from '@mui/icons-material/Image';
 import firebaseService from '../../../api-service/firebaseService';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserInfo } from '../../../store/UserSlice';
 function SendMessage() {
+  const { chatId } = useParams();
   const [message, setMessage] = useState("");
+  const user_info = useSelector(selectUserInfo);
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      sendMessage();
+    }
+  };
+
+  const sendMessage = () => {
+    if (message.trim() !== '') {
+      firebaseService.sendMessage1(chatId, user_info, message);
+      setMessage('');
+    }
+  };
   return (
     <TextField
         className='input-send-container'
@@ -15,6 +33,7 @@ function SendMessage() {
         variant="outlined"
         value = {message}
         onChange={(e) => setMessage(e.target.value)}
+        onKeyPress={handleKeyPress}
         fullWidth
         InputProps={{
         endAdornment: (
@@ -22,12 +41,9 @@ function SendMessage() {
                 <Box display="flex" alignItems="center">
                     <LinkIcon className='link-icon icon'/>
                     <ImageIcon className='image-icon icon'/>
-                    <SendIcon className='send-icon icon' onClick={(e) =>{
-                      e.preventDefault();
-                      alert("dsadasdasdsdas");
-                      // firebaseService.initializeConversation("aaaaaaaaaaaa", '2', '1');
-                      firebaseService.sendMessage1("aaaaaaaaaaaa", '2', '1', "aaaaabbbbbba");
-                    }}/>
+                    <SendIcon className='send-icon icon' 
+                    onClick={sendMessage}
+                    />
                 </Box>
           </InputAdornment>
         ),
