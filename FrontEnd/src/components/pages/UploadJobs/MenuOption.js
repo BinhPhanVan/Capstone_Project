2158a4299c0b82c7358b186596e9f9407cc5888d
BinhPhanVan <phanvanbinh192837@gmail.com>
2023-06-08 00:30:08 +0700

    Implement update and delete job with recruiter

--- FrontEnd/src/components/pages/UploadJobs/MenuOption.js
+++ FrontEnd/src/components/pages/UploadJobs/MenuOption.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useDispatch } from 'react-redux';
+import { delete_job, update_job_status } from '../../../store/JobSlice';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 export default function MenuOption({job}) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
@@ -13,6 +19,27 @@ export default function MenuOption({job}) {
   const handleClose = (event) => {
     setAnchorEl(null);
     event.stopPropagation();
+  };
+  const handleUpdateStatus = async (job) => {
+    const actionResult = await dispatch(update_job_status(job.id));
+    if (update_job_status.fulfilled.match(actionResult)) {
+      toast.success(actionResult.payload['message']);
+      navigate("/recruiter/upload-jobs");
+    }
+    if (update_job_status.rejected.match(actionResult)) {
+      toast.error(actionResult.payload['message']);
+    }
+  };
+
+  const handleDeleteJob = async (job) => {
+    const actionResult = await dispatch(delete_job(job.id));
+    if (delete_job.fulfilled.match(actionResult)) {
+      toast.success("Deleted successfully");
+      navigate("/recruiter/upload-jobs");
+    }
+    if (delete_job.rejected.match(actionResult)) {
+      toast.error("Delete failed");
+    }
   };
 
   return (
@@ -29,9 +56,9 @@ export default function MenuOption({job}) {
       >
         <MenuItem onClick={handleClose}>Edit Job</MenuItem>
         {job.active 
-        ?<MenuItem onClick={handleClose}>Deactive</MenuItem>
-        :<MenuItem onClick={handleClose}>Active</MenuItem>}
-        <MenuItem onClick={handleClose}>Delete Job</MenuItem>
+        ?<MenuItem onClick={() => handleUpdateStatus(job)}>Deactive</MenuItem>
+        :<MenuItem onClick={() => handleUpdateStatus(job)}>Active</MenuItem>}
+        <MenuItem onClick={() => handleDeleteJob(job)}>Delete Job</MenuItem>
       </Menu>
     </div>
   );
--- FrontEnd/src/store/JobSlice.js
+++ FrontEnd/src/store/JobSlice.js
@@ -41,10 +41,37 @@ export const get_all_jobs_owner = createAsyncThunk(
   }
 );
 
+export const update_job_status = createAsyncThunk(
+  "recruiter/update-job",
+  async (data, { rejectWithValue }) => {
+    try {
+        const res = await jobService.update_job_status(data);
+        return res;
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue("Access denied! Please try again.");
+    }
+  }
+);
+let idJobDeleted;
+export const delete_job = createAsyncThunk(
+  "recruiter/delete-job",
+  async (data, { rejectWithValue }) => {
+    try {
+        const res = await jobService.delete_job(data);
+        idJobDeleted = data;
+        return res;
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue("Access denied! Please try again.");
+    }
+  }
+);
+
 const initialState = {
   isLoading: false,
   jobs : [],
-  jobs_owner: []
+  jobs_owner: [],
 };
 
 const jobSlice = createSlice({
@@ -83,6 +110,34 @@ const jobSlice = createSlice({
     builder.addCase(get_all_jobs_owner.fulfilled, (state, action) => {
       state.isLoading = false;
       state.jobs_owner = action.payload;
+    });
+    builder.addCase(update_job_status.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(update_job_status.rejected, (state, action) => {
+      state.isLoading = false;
+    });
+    builder.addCase(update_job_status.fulfilled, (state, action) => {
+      state.isLoading = false;
+      const id = action.payload.data;
+      const jobIndex = state.jobs_owner.findIndex(job => job.id === id);
+      if (jobIndex !== -1) {
+        state.jobs_owner[jobIndex]['active'] = !state.jobs_owner[jobIndex]['active'];
+      }
+    });
+    builder.addCase(delete_job.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(delete_job.rejected, (state, action) => {
+      state.isLoading = false;
+    });
+    builder.addCase(delete_job.fulfilled, (state, action) => {
+      state.isLoading = false;
+      const id = idJobDeleted;
+      const jobIndex = state.jobs_owner.findIndex(job => job.id === id);
+      if (jobIndex !== -1) {
+        state.jobs_owner.splice(jobIndex, 1);
+      }
     });
   },
 });
