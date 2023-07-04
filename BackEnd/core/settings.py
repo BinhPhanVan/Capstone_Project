8fcd584cd92b0a8e7acf74a58828eb5b2e27516f
BinhPhanVan <phanvanbinh192837@gmail.com>
2023-07-04 21:39:58 +0700

    maintain for email hireit

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -163,8 +163,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = 'chatgptbp21@gmail.com'
-EMAIL_HOST_PASSWORD = 'urcbipmjgbrcotbc'
+EMAIL_HOST_USER = 'hireit.vn@gmail.com'
+# EMAIL_HOST_PASSWORD = 'urcbipmjgbrcotbc'
+EMAIL_HOST_PASSWORD = 'hmihzaoisywnbxcz'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
